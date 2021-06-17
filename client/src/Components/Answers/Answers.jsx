385e48d730cbea50f4ca4d3f8ff2222e0bbef7ac
Ashley Bennett <bennettashley2011@gmail.com>
2021-06-17 11:58:42 +0100

    improved Styling

--- client/src/Components/Answers/Answers.jsx
+++ client/src/Components/Answers/Answers.jsx
@@ -6,12 +6,17 @@ import {
   postAnswersForQuestion,
 } from "../../Utils/Axios";
 import {
-  Card,
-  CardContent,
-  CardActions,
-  Typography,
   Button,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  Divider,
 } from "@material-ui/core";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import SaveIcon from "@material-ui/icons/Save";
+
+import "./Answers.css";
 
 const Answers = (props) => {
   const [answers, setAnswers] = useState([]);
@@ -105,78 +110,122 @@ const Answers = (props) => {
   };
 
   return (
-    <div>
+    <div className="answersContainer">
       {props.authLevel > 2 && (
-        <div>
-          <label htmlFor="question">Question:</label>
-          <input
-            type="text"
-            value={question}
-            onChange={(e) => handleQuestionChange(e)}
-          />
-          {answers.map((answer, index) => {
-            return (
-              <div>
-                <label htmlFor="answer">Answer</label>
-                <input
-                  type="text"
-                  value={answer.Answer}
-                  onChange={(e) => {
-                    handleChangeAnswer(e, index);
-                  }}
-                />
-                <input
-                  type="checkbox"
-                  name=""
-                  id=""
-                  checked={answer.IsCorrect}
-                  value={answer.IsCorrect}
-                  onChange={(e) => {
-                    handleChangeAnswerCorrect(e, index);
-                  }}
-                />
-                {answers.length > 3 ? (
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => {
-                      handleDeleteAnswer(index);
-                    }}
-                  >
-                    Delete
-                  </Button>
-                ) : null}
-              </div>
-            );
-          })}
-          {answers.length < 5 ? (
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleNewAnswer}
-            >
-              Add Answer
-            </Button>
-          ) : null}
-          <Button variant="contained" color="primary" onClick={handleUpdate}>
-            Update
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleDeleteQuestion}
-          >
-            Delete Question
-          </Button>
+        <div className="answersEditContainer">
+          <div className="answersTopRow">
+            <TextField
+              className="answers_questionInput"
+              label="Question"
+              value={question}
+              onChange={(e) => handleQuestionChange(e)}
+            />
+          </div>
+          <ol className="answersMidRow" type="A">
+            {answers.map((answer, index) => {
+              return (
+                <>
+                  <li>
+                    <div className="answers_answers">
+                      <TextField
+                        label="Answer"
+                        value={answer.Answer}
+                        onChange={(e) => {
+                          handleChangeAnswer(e, index);
+                        }}
+                      />
+                      <div className="answers_checkbox">
+                        {index === 0 ? (
+                          <FormControlLabel
+                            value=""
+                            control={
+                              <Checkbox
+                                color="primary"
+                                checked={answer.IsCorrect}
+                                value={answer.IsCorrect}
+                                onChange={(e) => {
+                                  handleChangeAnswerCorrect(e, index);
+                                }}
+                                inputProps={{
+                                  "aria-label": "primary checkbox",
+                                }}
+                              />
+                            }
+                            label="Is A Correct Answer"
+                            labelPlacement="top"
+                          />
+                        ) : (
+                          <Checkbox
+                            color="primary"
+                            checked={answer.IsCorrect}
+                            value={answer.IsCorrect}
+                            onChange={(e) => {
+                              handleChangeAnswerCorrect(e, index);
+                            }}
+                            inputProps={{ "aria-label": "primary checkbox" }}
+                          />
+                        )}
+                      </div>
+                      {answers.length > 3 ? (
+                        <Button
+                          variant="outlined"
+                          style={{
+                            borderColor: "#d11a2a",
+                            color: "#d11a2a",
+                          }}
+                          onClick={() => {
+                            handleDeleteAnswer(index);
+                          }}
+                        >
+                          <DeleteForeverIcon />
+                        </Button>
+                      ) : null}
+                    </div>
+                  </li>
+                  <Divider />
+                </>
+              );
+            })}
+          </ol>
+          <div className="answersBottomRow">
+            {answers.length < 5 ? (
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={handleNewAnswer}
+              >
+                Add Answer
+                <AddCircleOutlineIcon style={{margin: "0 0 0 10"}} />
+              </Button>
+            ) : null}
+            <div className="answersBottomRow_standard">
+              <Button
+                variant="contained"
+                style={{ backgroundColor: "#4caf50", color: "#ffffff" }}
+                onClick={handleUpdate}
+              >
+                Save And Update <SaveIcon style={{margin: "0 0 0 10"}} />
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ backgroundColor: "#d11a2a", color: "#ffffff" }}
+                onClick={handleDeleteQuestion}
+              >
+                Delete Question <DeleteForeverIcon  style={{margin: "0 0 0 10"}}/>
+              </Button>
+            </div>
+          </div>
         </div>
       )}
       {props.authLevel === 2 && (
-        <div>
+        <ol type="A">
           {answers.map((answer) => {
-            return <p>{answer.Answer}</p>;
+            return <li>{answer.Answer}</li>;
           })}
-        </div>
+        </ol>
       )}
+      
     </div>
   );
 };
